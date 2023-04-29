def0c53e8054a104c87df5b86c0b5b7a0aab6642
Hiroto Kitamura <kirikonknow@gmail.com>
2023-04-30 06:56:50 +0900

    add super member.

--- src/ch1/librarian.py
+++ src/ch1/librarian.py
@@ -2,16 +2,11 @@ from typing import List
 
 from src.ch1.book_lending import BookLending
 from src.ch1.member import Member
+from src.ch1.user_with_block_member_right import UserWithBlockMemberRight
 from src.ch1.user_with_book_item_right import UserWithBookItemRight
 
 
-class Librarian(UserWithBookItemRight):
-
-    def block_member(self, member) -> bool:
-        pass
-
-    def unblock_member(self, member) -> bool:
-        pass
+class Librarian(UserWithBookItemRight, UserWithBlockMemberRight):
 
     def get_book_lendings_of_member(self, member: Member) -> List[BookLending]:
         pass
---
+++ src/ch1/user_with_block_member_right.py
@@ -0,0 +1,7 @@
+class UserWithBlockMemberRight:
+
+    def block_member(self, member) -> bool:
+        pass
+
+    def unblock_member(self, member) -> bool:
+        pass
