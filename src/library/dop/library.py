80d5b2086470c30bc8bd5e8bcfe4e225c3dfeeba
Hiroto Kitamura <kirikonknow@gmail.com>
2023-05-01 06:33:40 +0900

    add get_book_lendings methods(oop and dop)

---
+++ src/library/dop/library.py
@@ -0,0 +1,9 @@
+def get_book_lendings(libraryData, user_id, member_id):
+    """
+    会員が借りている本の一覧を取得する
+    :param libraryData: libraryの状態
+    :param user_id: 司書ID
+    :param member_id: 会員ID
+    :return: 会員が借りている本の一覧
+    """
+    pass
