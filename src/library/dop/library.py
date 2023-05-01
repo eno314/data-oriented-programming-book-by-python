db3092f56435f13e06ffb782924698b75b101913
Hiroto Kitamura <kirikonknow@gmail.com>
2023-05-01 09:11:29 +0900

    fix getter

--- src/library/dop/library.py
+++ src/library/dop/library.py
@@ -7,8 +7,7 @@ def search_book(library_data, search_query):
     :param library_data: libraryの状態
     :param search_query: 検索クエリ
     """
-    catalog_data = get_catalog_data_from(library_data)
-    return catalog.search_book(catalog_data, search_query)
+    return catalog.search_book(library_data['catalog_data'], search_query)
 
 
 def add_book_item(library_data, user_id, book_item_info):
@@ -18,10 +17,8 @@ def add_book_item(library_data, user_id, book_item_info):
     :param user_id: 司書ID
     :param book_item_info: 本の情報
     """
-    user_management_data = get_user_management_data_from(library_data)
-    if user_management.is_librarian(user_management_data, user_id):
-        catalog_data = get_catalog_data_from(library_data)
-        catalog.add_book_item(catalog_data, book_item_info)
+    if user_management.is_librarian(library_data['user_management_data'], user_id):
+        catalog.add_book_item(library_data['catalog_data'], book_item_info)
 
 
 def block_member(library_data, member_id):
@@ -30,8 +27,7 @@ def block_member(library_data, member_id):
     :param library_data: libraryの状態
     :param member_id: 会員ID
     """
-    user_management_data = get_user_management_data_from(library_data)
-    user_management.block_member(user_management_data, member_id)
+    user_management.block_member(library_data['user_management_data'], member_id)
 
 
 def unblock_member(library_data, member_id):
@@ -40,8 +36,7 @@ def unblock_member(library_data, member_id):
     :param library_data: libraryの状態
     :param member_id: 会員ID
     """
-    user_management_data = get_user_management_data_from(library_data)
-    user_management.unblock_member(user_management_data, member_id)
+    user_management.unblock_member(library_data['user_management_data'], member_id)
 
 
 def login(library_data, login_info):
@@ -50,21 +45,18 @@ def login(library_data, login_info):
     :param library_data: libraryの状態
     :param login_info: ログイン情報
     """
-    user_management_data = get_user_management_data_from(library_data)
-    user_management.login(user_management_data, login_info)
+    user_management.login(library_data['user_management_data'], login_info)
 
 
-def get_book_lendings(libraryData, user_id, member_id):
+def get_book_lendings(library_data, user_id, member_id):
     """
     会員が借りている本の一覧を取得する
-    :param libraryData: libraryの状態
+    :param library_data: libraryの状態
     :param user_id: 司書ID
     :param member_id: 会員ID
     """
-    user_management_data = get_user_management_data_from(library_data)
-    if user_management.is_librarian(user_management_data, user_id):
-        catalog_data = get_catalog_data_from(library_data)
-        return catalog.get_book_lendings(catalog_data, member_id)
+    if user_management.is_librarian(library_data['user_management_data'], user_id):
+        return catalog.get_book_lendings(library_data['catalog_data'], member_id)
     else:
         raise Exception("You don't have permission to get book lendings.")
 
@@ -76,10 +68,8 @@ def checkout_book(library_data, user_id, book_item_id):
     :param user_id: 司書ID
     :param book_item_id: 本のID
     """
-    user_management_data = get_user_management_data_from(library_data)
-    if user_management.is_librarian(user_management_data, user_id):
-        catalog_data = get_catalog_data_from(library_data)
-        catalog.checkout_book(catalog_data, book_item_id)
+    if user_management.is_librarian(library_data['user_management_data'], user_id):
+        catalog.checkout_book(library_data['catalog_data'], book_item_id)
 
 
 def return_book(library_data, user_id, book_item_id):
@@ -89,7 +79,5 @@ def return_book(library_data, user_id, book_item_id):
     :param user_id: 司書ID
     :param book_item_id: 本のID
     """
-    user_management_data = get_user_management_data_from(library_data)
-    if user_management.is_librarian(user_management_data, user_id):
-        catalog_data = get_catalog_data_from(library_data)
-        catalog.return_book(catalog_data, book_item_id)
+    if user_management.is_librarian(library_data['user_management_data'], user_id):
+        catalog.return_book(library_data['catalog_data'], book_item_id)
