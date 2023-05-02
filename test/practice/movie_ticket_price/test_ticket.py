5ff5eba4304ac0f7f29b5ed6ace202bc3d0c55bd
Hiroto Kitamura <kirikonknow@gmail.com>
2023-05-02 14:07:10 +0900

    refactor test code.

--- test/practice/movie_ticket_price/test_ticket.py
+++ test/practice/movie_ticket_price/test_ticket.py
@@ -6,60 +6,35 @@ from pyrsistent import pmap
 from src.practice.movie_ticket_price import ticket
 from src.practice.movie_ticket_price.data.ticket import ticket_data
 
+screening_datetime_test_data = {
+    'weekday_daytime': datetime.datetime(2023, 4, 28, 10, 0, 0),
+    'weekday_late_show': datetime.datetime(2023, 5, 2, 23, 0, 0),
+    'weekend_daytime': datetime.datetime(2023, 4, 30, 10, 0, 0),
+    'weekend_late_show': datetime.datetime(2023, 5, 6, 23, 0, 0),
+    'movie_day_daytime': datetime.datetime(2023, 5, 1, 10, 0, 0),
+    'movie_day_late_show': datetime.datetime(2023, 5, 1, 23, 0, 0),
+}
+
 
 @pytest.fixture
-def test_data():
-    return {
-        'general_user': pmap({
-            'age': 30,
-            'is_student': False,
-            'is_middle_or_high_school': False,
-            'is_elementary_school_or_younger': False,
-            'has_disability': False,
-            'is_member': False,
-        }),
-        'normal_day_daytime': datetime.datetime(2023, 5, 2, 10, 0, 0),
-        'normal_day_late_show': datetime.datetime(2023, 5, 2, 23, 0, 0),
-        'movie_day_daytime': datetime.datetime(2023, 5, 1, 10, 0, 0),
-        'movie_day_late_show': datetime.datetime(2023, 5, 1, 23, 0, 0),
-    }
+def general_user():
+    return pmap({
+        'age': 30,
+        'is_student': False,
+        'is_middle_or_high_school': False,
+        'is_elementary_school_or_younger': False,
+        'has_disability': False,
+        'is_member': False,
+    })
 
 
-def test_general_weekday_daytime(test_data):
-    actual = ticket.calculate_price(
-        ticket_data,
-        test_data['general_user'],
-        test_data['normal_day_daytime']
-    )
-    expected = pmap({'price': 1900, 'currency': 'JPY'})
-    assert expected == actual
-
-
-def test_general_weekday_late_show(test_data):
-    actual = ticket.calculate_price(
-        ticket_data,
-        test_data['general_user'],
-        test_data['normal_day_late_show']
-    )
-    expected = pmap({'price': 1400, 'currency': 'JPY'})
-    assert expected == actual
-
-
-def test_general_weekday_movie_day_daytime(test_data):
-    actual = ticket.calculate_price(
-        ticket_data,
-        test_data['general_user'],
-        test_data['movie_day_daytime']
-    )
-    expected = pmap({'price': 1200, 'currency': 'JPY'})
-    assert expected == actual
-
-
-def test_general_weekday_movie_day_late_show(test_data):
-    actual = ticket.calculate_price(
-        ticket_data,
-        test_data['general_user'],
-        test_data['movie_day_late_show']
-    )
-    expected = pmap({'price': 1200, 'currency': 'JPY'})
-    assert expected == actual
+@pytest.mark.parametrize('screening_datetime, expected', [
+    (screening_datetime_test_data['weekday_daytime'], pmap({'price': 1900, 'currency': 'JPY'})),
+    (screening_datetime_test_data['weekday_late_show'], pmap({'price': 1400, 'currency': 'JPY'})),
+    (screening_datetime_test_data['weekend_daytime'], pmap({'price': 1900, 'currency': 'JPY'})),
+    (screening_datetime_test_data['weekend_late_show'], pmap({'price': 1400, 'currency': 'JPY'})),
+    (screening_datetime_test_data['movie_day_daytime'], pmap({'price': 1200, 'currency': 'JPY'})),
+    (screening_datetime_test_data['movie_day_late_show'], pmap({'price': 1200, 'currency': 'JPY'})),
+])
+def test_general_weekday_daytime(general_user, screening_datetime, expected):
+    assert expected == ticket.calculate_price(ticket_data, general_user, screening_datetime)
